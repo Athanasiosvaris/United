1941914f55e04830ca5220f5cbb8a876af9990f9
DVasios <dimitrisvasios99@gmail.com>
2023-02-19 14:42:49 +0200

    Changes in the home_page

--- lib/home_page.dart
+++ lib/home_page.dart
@@ -8,11 +8,13 @@ class HomePage extends StatefulWidget {
 }
 
 class _HomePage extends State<HomePage> {
+  int currentPageIndex = 0;
   @override
   Widget build(BuildContext context) {
     return Scaffold(
       backgroundColor: const Color.fromARGB(255, 202, 196, 208),
       appBar: AppBar(
+          automaticallyImplyLeading: false,
           title: const Text('Welcome Dimitris',
               style: TextStyle(
                   fontSize: 20,
@@ -25,11 +27,42 @@ class _HomePage extends State<HomePage> {
             children: <Widget>[
               Container(
                 padding: const EdgeInsets.all(20),
-                child: const SizedBox(
-                  width: 200, height: 50),
+                child: const SizedBox(width: 200, height: 50),
               )
             ],
           )),
+      bottomNavigationBar: NavigationBar(
+          destinations: const <Widget>[
+            NavigationDestination(
+                label: 'Home', icon: Icon(Icons.home, color: Colors.white60)),
+            NavigationDestination(
+                label: 'Discover',
+                icon: Icon(Icons.explore, color: Colors.white60)),
+            NavigationDestination(
+                label: 'Leaderboard',
+                icon: Icon(
+                  Icons.leaderboard,
+                  color: Colors.white60,
+                )),
+            NavigationDestination(
+                label: 'Profile',
+                icon: Icon(
+                  Icons.person,
+                  color: Colors.white60,
+                ))
+          ],
+          backgroundColor: const Color.fromARGB(255, 73, 69, 79),
+          elevation: 10,
+          onDestinationSelected: (int index) {
+            setState(() {
+              switch (index) {
+                case 0:
+                case 1:
+                case 2:
+                case 3:
+              }
+            });
+          }),
     );
   }
 }
