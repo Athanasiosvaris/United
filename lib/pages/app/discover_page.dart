79b32d11353aa8a6ba56094704e07a4651432293
ntua-el19606 <thanosv22@gmail.com>
2023-02-21 14:35:04 +0200

    Search bar

--- lib/pages/app/discover_page.dart
+++ lib/pages/app/discover_page.dart
@@ -1,3 +1,4 @@
+//https://www.youtube.com/watch?v=jKlJ9MC4nS8 => flutter search bar tutorial
 import 'package:flutter/material.dart';
 
 class DiscoverPage extends StatelessWidget {
@@ -7,12 +8,83 @@ class DiscoverPage extends StatelessWidget {
   Widget build(BuildContext context) {
     return Scaffold(
         appBar: AppBar(
-            automaticallyImplyLeading: false,
-            title: const Text('Welcome Dimitris',
-                style: TextStyle(
-                    fontSize: 20,
-                    fontWeight: FontWeight.bold,
-                    color: Colors.black)),
-            backgroundColor: const Color.fromARGB(255, 89, 61, 167)));
+      automaticallyImplyLeading: false,
+      title: const Text('Welcome Dimitris',
+          style: TextStyle(
+              fontSize: 20, fontWeight: FontWeight.bold, color: Colors.black)),
+      backgroundColor: const Color.fromARGB(255, 89, 61, 167),
+      actions: [
+        IconButton(
+            onPressed: () {
+              showSearch(context: context, delegate: CustomSearch());
+            },
+            icon: const Icon(Icons.search))
+      ],
+    ));
+  }
+}
+
+class CustomSearch extends SearchDelegate {
+  List<String> alldata = [
+    'Athanasios Varis',
+    'Giorgos Vlachopoulos',
+    'Dimitris Vasios'
+  ];
+
+  @override
+  List<Widget>? buildActions(BuildContext context) {
+    return [
+      IconButton(
+          onPressed: () {
+            query = '';
+          },
+          icon: const Icon(Icons.clear))
+    ];
+  }
+
+  @override
+  Widget? buildLeading(BuildContext context) {
+    return IconButton(
+      icon: const Icon(Icons.arrow_back),
+      onPressed: () {
+        close(context, null);
+      },
+    );
+  }
+
+  @override
+  Widget buildSuggestions(BuildContext context) {
+    List<String> matchQuery = [];
+    for (var item in alldata) {
+      if (item.toLowerCase().contains(query.toLowerCase())) {
+        matchQuery.add(item);
+      }
+    }
+    return ListView.builder(
+        itemCount: matchQuery.length,
+        itemBuilder: (context, index) {
+          var result = matchQuery[index];
+          return ListTile(
+            title: Text(result),
+          );
+        });
+  }
+
+  @override
+  Widget buildResults(BuildContext context) {
+    List<String> matchQuery = [];
+    for (var item in alldata) {
+      if (item.toLowerCase().contains(query.toLowerCase())) {
+        matchQuery.add(item);
+      }
+    }
+    return ListView.builder(
+        itemCount: matchQuery.length,
+        itemBuilder: (context, index) {
+          var result = matchQuery[index];
+          return ListTile(
+            title: Text(result),
+          );
+        });
   }
 }
