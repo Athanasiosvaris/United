eaf01171d9cc8b3e2afdac5ebe2505ddf4bb4087
ntua-el19606 <thanosv22@gmail.com>
2023-02-22 17:41:53 +0200

    profile page

--- lib/pages/app/profile.dart
+++ lib/pages/app/profile.dart
@@ -164,6 +164,152 @@ class _ProfilePage extends State<ProfilePage>
                   ),
                 ],
               ),
+              SizedBox(height: 20),
+              Row(
+                mainAxisAlignment: MainAxisAlignment.start,
+                children: [
+                  Container(
+                    width: 380,
+                    decoration: BoxDecoration(
+                      color: Colors.grey,
+                      borderRadius: BorderRadius.circular(10),
+                    ),
+                    child: Column(
+                      children: [
+                        Row(
+                          mainAxisAlignment: MainAxisAlignment.start,
+                          // ignore: prefer_const_literals_to_create_immutables
+                          children: [
+                            Padding(
+                                padding: EdgeInsets.all(5.0),
+                                child: Icon(Icons.home_work_outlined)),
+                            Text('Athens'),
+                          ],
+                        ),
+                        Row(
+                          mainAxisAlignment: MainAxisAlignment.start,
+                          // ignore: prefer_const_literals_to_create_immutables
+                          children: [
+                            Padding(
+                              padding: EdgeInsets.all(5.0),
+                              child: Icon(Icons.school_outlined),
+                            ),
+                            Text(
+                                'School of Electrical & Computer engineering, NTUA '),
+                          ],
+                        ),
+                        Row(
+                          mainAxisAlignment: MainAxisAlignment.start,
+                          // ignore: prefer_const_literals_to_create_immutables
+                          children: [
+                            Padding(
+                              padding: EdgeInsets.all(5.0),
+                              child: Icon(Icons.mail_outline),
+                            ),
+                            Text('dimitrisv@hotmail.com'),
+                          ],
+                        ),
+                        Row(
+                          mainAxisAlignment: MainAxisAlignment.start,
+                          // ignore: prefer_const_literals_to_create_immutables
+                          children: [
+                            Padding(
+                              padding: EdgeInsets.all(5.0),
+                              child: Icon(Icons.facebook_outlined),
+                            ),
+                            Text('https://'),
+                          ],
+                        ),
+                      ],
+                    ),
+                  ),
+                ],
+              ),
+              SizedBox(height: 15),
+              Row(
+                children: [
+                  Container(
+                    decoration: BoxDecoration(
+                      color: Color.fromARGB(255, 101, 98, 108),
+                      borderRadius:
+                          BorderRadius.circular(10), // Set the radius to 10
+                    ),
+                    width: 70,
+                    height: 20,
+                    child: Text(
+                      "Interests",
+                      textAlign: TextAlign.center,
+                      // ignore: prefer_const_constructors
+                      style: TextStyle(
+                        fontFamily: 'Roboto',
+                        color: Colors.white,
+                        fontWeight: FontWeight.w500,
+                      ),
+                    ),
+                  ),
+                ],
+              ),
+              SizedBox(height: 15),
+              Row(
+                mainAxisAlignment: MainAxisAlignment.start,
+                children: [
+                  Container(
+                    width: 380,
+                    decoration: BoxDecoration(
+                      color: Colors.grey,
+                      borderRadius: BorderRadius.circular(10),
+                    ),
+                    child: Column(
+                      children: [
+                        Row(
+                          mainAxisAlignment: MainAxisAlignment.start,
+                          // ignore: prefer_const_literals_to_create_immutables
+                          children: [
+                            Padding(
+                                padding: EdgeInsets.all(5.0),
+                                child: Icon(Icons.home_work_outlined)),
+                            Text('Athens'),
+                          ],
+                        ),
+                        Row(
+                          mainAxisAlignment: MainAxisAlignment.start,
+                          // ignore: prefer_const_literals_to_create_immutables
+                          children: [
+                            Padding(
+                              padding: EdgeInsets.all(5.0),
+                              child: Icon(Icons.school_outlined),
+                            ),
+                            Text(
+                                'School of Electrical & Computer engineering, NTUA '),
+                          ],
+                        ),
+                        Row(
+                          mainAxisAlignment: MainAxisAlignment.start,
+                          // ignore: prefer_const_literals_to_create_immutables
+                          children: [
+                            Padding(
+                              padding: EdgeInsets.all(5.0),
+                              child: Icon(Icons.mail_outline),
+                            ),
+                            Text('dimitrisv@hotmail.com'),
+                          ],
+                        ),
+                        Row(
+                          mainAxisAlignment: MainAxisAlignment.start,
+                          // ignore: prefer_const_literals_to_create_immutables
+                          children: [
+                            Padding(
+                              padding: EdgeInsets.all(5.0),
+                              child: Icon(Icons.facebook_outlined),
+                            ),
+                            Text('https://'),
+                          ],
+                        ),
+                      ],
+                    ),
+                  ),
+                ],
+              ),
             ],
           ),
         ),
